19839dd3d66761599de3bbeca1c546e072e426df
Varshini Neelamegan <varshini2010.nm@gmail.com>
2026-06-12 19:54:54 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,21 @@
+x=input("enter your name:")
+rep=0
+print("hello",x,"i am so happy to meet you")
+y=input("'can we be friends?answer with yes or no:'")
+while y!="yes":
+            if rep==0:
+              print("pleaseeeeeeee")
+            if rep==1:
+              print("iam really sorry")
+            if rep==2:
+              print("you are my best friend") 
+            if rep==3:
+              print("i will do anything for you") 
+            if rep==4:
+              print("don't say no") 
+            if rep==5:
+              print("i won't let you go")
+            y=input("will you be my friend?")
+            rep+=1
+else:       
+       print("yayyy...i am so happy to have you as my friend")
